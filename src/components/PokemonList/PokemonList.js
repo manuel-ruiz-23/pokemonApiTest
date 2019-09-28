a80714c8bb018a396be5b0c766ba556bea510479
manuel-ruiz-23 <rrmanuel1123@gmail.com>
2019-09-27 23:56:26 -0600

    pokemon list base

--- src/components/PokemonList/PokemonList.js
+++ src/components/PokemonList/PokemonList.js
@@ -1,42 +1,55 @@
-import React, {useState} from 'react';
-import useDataApi from 'use-data-api';
+import React, { useState, useEffect, Fragment} from 'react';
 import { Link } from "react-router-dom";
 
+import styles from './PokemonList.module.css';
+
+const usePokemonList = () => {
+    const [pokemons, setPokemons] = useState([])
+
+    useEffect(() => {
+        async function fetchData() {
+            const response = await fetch('https://pokeapi.co/api/v2/pokemon/')
+                .then(response => {
+                    return response.json();
+                });
+
+
+            // console.log('hola',response);
+            setPokemons(response.results);
+        }  
+        
+        fetchData();
+    },[]);
+
+    return pokemons;
+}
+
+
 const  PokemonList = () => {
-
-    const startUrl = 'https://pokeapi.co/api/v2/pokemon/'
-
-    const [currentUrl, setCurrentUrl] = useState(startUrl);
-
-    
-    const [{ data, isLoading, isError }, doFetch] = useDataApi(
-        currentUrl,
-        null,
-      );
-
-    const handleNextUrL = () =>{
-        const nextUrl = data.next;
-        setCurrentUrl(nextUrl);
-    }
+    const pokemons = usePokemonList();
 
     return(
-        <div>
-            <ul>
-            {data && (
-                data.results.map((pokemon) => (
-                    <li>
-                        <Link to={
-                            {
-                                pathname: "/pokemon",
-                                state: { url: pokemon.url}
-                            }
-                        }>{pokemon.name}</Link>
-                    </li>   
-                ))
-            )}
-            </ul>
-        </div>
+        <Fragment>
+            <PokemonListView pokemons={pokemons}/>
+        </Fragment>
+        
     );
 };
+
+const PokemonListView = ({ pokemons }) => (
+    <div className={styles.listContainer}>
+        <ul>
+            {pokemons.map((pokemon, index) => (
+                <Link key={index} to={`/${pokemon.name}`}>
+                    <li>{pokemon.name}</li>
+                </Link>
+            ))}
+        </ul>
+    </div>
+);
+
+
+
+
 
 export { PokemonList };
